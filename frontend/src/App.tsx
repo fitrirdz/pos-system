892dc637d07f058e899961ee66697a0ea1acebf4
fitrirdz <fratnadewi@gmail.com>
2026-02-16 15:19:18 +0700

    feat: add initial frontend code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-slate-100'>
+      <h1 className='text-3xl font-bold text-indigo-600'>
+        POS System Ready 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
